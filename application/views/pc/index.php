39c263348035d7621c391523bcd9d505690dcb12
13520489@gm.uit.edu.vn <13520489@gm.uit.edu.vn>
2015-11-10 00:26:00 +0700

    giaodien

--- application/views/pc/index.php
+++ application/views/pc/index.php
@@ -1,21 +1,27 @@
-﻿<div class="title"><h1 style="color:#0923EC; font-size:400%; margin-top:10%; margin-left:38%">Smart Search</h1></div>
+﻿<html>
+<head>
+		<title></title>
+		<link type="text/css"  rel="stylesheet"  href="<?= base_url(); ?>statics/pc/css_styles.css" />
+	</head>
+<body>
+<div class="title"><h1 >Smart Search</h1></div>
 	<div class="search_form">
-	<input  style="width:15cm; height:1cm; margin-left:10cm"id="id_articles_keyword" type="text" class="search_text" placeholder="Tìm bài viết"  />
-     <button  style="color:#DC22FD; height:1.5cm; width:4cm; background-color:#E8C6C8 "> SEARCH</button> 
+	  <input type="text" class="search_text" placeholder="Tìm bài viết"  />
+     	  <button  > SEARCH</button> 
 	</div>
 
-<div class="dev_team " style="margin-left:47%; color:#0115F7">
+<div class="dev_team " >
 	<h2 >Developers</h2>
 	<h3>Rye Nguyen</h3>
 	<h3>Shichiki Le</h3>
 	<h3>Amy Le</h3>
 	<h3>Fi Nguyen</h3>
 </div>
-
-</br></br>
 <div class="crawl_button">
-	<button style="color:#F90E12; height:3cm; width:7cm; background:#52B46F; float:right; font-size:200%">Crawl HTML</button>
+	<button >Crawl HTML</button>
 </div>
 <div class="analyze_button">
-<button style="color:#F90E12; height:3cm; width:7cm; background:#52B46F; float:left; font-size:200%  " > Analyze </button>
+<button  > Analyze </button>
 </div>
+</body>
+<html>
